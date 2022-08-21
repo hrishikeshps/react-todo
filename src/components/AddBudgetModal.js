2500b5b36e4a98e372fcd37a38836edd4706b204
Hrishikesh <hrishikeshps@gmail.com>
2022-08-21 18:13:15 +0530

    adding context and add budget functionality

---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,43 @@
+import { useRef } from "react"
+import { Modal, Form, Button } from "react-bootstrap"
+import { useBudgets } from "../context/BudgetsContext"
+
+function AddBudgetModal({show, handleClose}) {
+    const nameRef = useRef();
+    const maxRef = useRef();
+    const { addBudget } = useBudgets();
+    function handleSubmit(e){
+        e.preventDefault()
+        addBudget({
+            name: nameRef.current.value,
+            max: parseFloat(maxRef.current.value)
+        })
+        handleClose()
+    }
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+        <Form onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+                <Modal.Title>New Budget</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter name" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="max">
+                    <Form.Label>Max Spending</Form.Label>
+                    <Form.Control ref={maxRef} type="number" placeholder="Enter max" required min={0} step={0.01}/>
+                </Form.Group>
+                <div className="d-flex justify-content-end">
+                    <Button variant="primary" type="submit">Add</Button>
+                    <Button variant="secondary" className="ml-1" type="submit">Cancel</Button>
+                </div>
+            </Modal.Body>
+        </Form>
+    </Modal>
+  )
+}
+
+export default AddBudgetModal
--- src/pages/BudgetApp.js
+++ src/pages/BudgetApp.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { Stack, Button, Container } from "react-bootstrap"
+import AddBudgetModal from '../components/AddBudgetModal'
 import BudgetCard from '../components/BudgetCard'
+import { useState } from 'react'
 
 function BudgetApp() {
+    const [showAddBudgetModal, setShowAddBudgetModal] = React.useState(false);
+
     return (
-        <Container className="my-4">
-            BudgetApp
-            <Stack direction='horizontal' gap="2" className="mb-4">
-                <h1 className="me-auto">Budgets</h1>
-                <Button variant="primary">Add Budget</Button>
-                <Button variant="outline-primary">Add Expense</Button>
-            </Stack>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(auto-fill,minmax(300px, 1fr)",
-                    gap: "1rem",
-                    alignItems: "flex-start"
-                }}>
-                <BudgetCard name="Entertainment" amount={780} max={500} gray ></BudgetCard>
-            </div>
-        </Container>
+        <>  
+            <Container className="my-4">
+                BudgetApp
+                <Stack direction='horizontal' gap="2" className="mb-4">
+                    <h1 className="me-auto">Budgets</h1>
+                    <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+                    <Button variant="outline-primary">Add Expense</Button>
+                </Stack>
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(auto-fill,minmax(300px, 1fr)",
+                        gap: "1rem",
+                        alignItems: "flex-start"
+                    }}>
+                    <BudgetCard name="Entertainment" amount={780} max={500} gray ></BudgetCard>
+                </div>
+            </Container>
+            <AddBudgetModal show={showAddBudgetModal} handleClose={() => {setShowAddBudgetModal(false)}}/>
+        </>
+        
     )
 }
 
